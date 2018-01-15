b6af51e2cac91fbcd2961d9a69e4f8b2fa729812
bennybop <bennybop74@yahoo.com>
2018-01-15 00:42:21 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -2,44 +2,45 @@ class MusicLibraryController
 
   def initialize(path = "./db/mp3s")
     importer = MusicImporter.new(path)
-    importer.import 
-  end 
+    importer.import
+  end
 
-  def call 
+  def call
     input = ""
     while input != "exit"
       puts "Welcome to Your Music Library!"
       puts "What would you like to do?"
-      input = gets.strip 
-      case input 
+      input = gets.strip
+      case input
       when "list songs"
-        songs 
+        songs
       when "list artists"
-        artists 
+        artists
       when "list genres"
-        genres 
+        genres
       when "list artist"
-        list_artist 
+        list_artist
       when "list genre"
-        list_genre 
+        list_genre
       when "play song"
-        play_song 
-      end 
-    end 
-  end 
+        play_song
+      end
+    end
+  end
 
-  def artists 
-    Artist.all.each.with_index(1) do |a, i| 
+  def artists
+    Artist.all.each.with_index(1) do |a, i|
       puts "#{i}. #{a}"
-    end 
-  end 
+    end
+  end
 
-  def genres 
+  def genres
     Genre.all.each.with_index(1) do |g, i|
       puts "#{i}. #{a}"
-    end 
-  end 
+    end
+  end
 
-  def list_artist 
+  def list_artist
     puts "What artist by name you like to "
-       
+  end 
+end 
