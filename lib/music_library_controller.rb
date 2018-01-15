8948b79681dbbdb114e7b095be8fc5c2de71731d
bennybop <bennybop74@yahoo.com>
2018-01-15 00:44:49 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -47,5 +47,29 @@ class MusicLibraryController
       artist.songs.each do |s,i|
         puts "#{i}. #{s}"
   end
-end 
+end
+end
+
+
+  def list_genre
+    puts "What genre by name you like to list songs for?"
+    genre_input = gets.strip
+    if genre = Genre.find_by_name(genre_input)
+      genre.songs.each do |s,i|
+        puts "#{i}. #{s}"
+      end
+    end
+  end
+
+  def play_song
+    puts "What song number would you like to play?"
+    song_input = gets.strip
+    puts "Playing #{Song.all[song_input.to_i-1]}"
+  end
+
+  def songs
+    Song.all.each.with_index(1) do |s, i|
+      puts "#{i}. #{s}"
+    end
+  end
 end
